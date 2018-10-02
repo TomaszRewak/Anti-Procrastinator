276b1796959747d517df185d91dafe413e2fae0c
Tomasz Rewak <tomaszrewak@gmail.com>
2018-10-02 21:28:55 +0200

    Add project files.

---
+++ WebsiteBlocker/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebsiteBlocker
+{
+	class Program
+	{
+		const string hostFilePath = @"C:\Windows\System32\drivers\etc\hosts";
+		const string banFilePath = @".\block-list";
+
+		static void Main(string[] args)
+		{
+			var websiteBlocker = new WebsiteBlocker(hostFilePath, banFilePath);
+
+			while(true)
+			{
+				websiteBlocker.UpdateBanList();
+				websiteBlocker.UpdateHostFile();
+
+				Thread.Sleep(5000);
+			}
+		}
+	}
+}
---
+++ WebsiteBlocker/WebsiteBlocker.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WebsiteBlocker
+{
+	internal class WebsiteBlocker
+	{
+		private const string motivator = "Get to work you piece of shit";
+
+		private readonly string hostFilePath;
+		private readonly string banFilePath;
+
+		ICollection<string> blockList = new List<string>();
+
+		DateTime lastBanUpdate = new DateTime(1990, 1, 1);
+		DateTime lastHostUpdate = new DateTime(1990, 1, 1);
+
+		public WebsiteBlocker(string hostFilePath, string banFilePath)
+		{
+			this.hostFilePath = hostFilePath;
+			this.banFilePath = banFilePath;
+		}
+
+		public void UpdateBanList()
+		{
+			if (File.GetLastWriteTime(banFilePath) > lastBanUpdate)
+			{
+				var banFile = File.ReadAllLines(banFilePath)
+					.Select(line => line.Trim())
+					.Where(line => !String.IsNullOrWhiteSpace(line))
+					.ToList();
+
+				foreach (var line in banFile)
+					if (!blockList.Contains(line))
+						blockList.Add(line);
+
+				lastBanUpdate = DateTime.Now;
+			}
+		}
+
+		public void UpdateHostFile()
+		{
+			if (File.GetLastWriteTime(hostFilePath) > lastHostUpdate || lastBanUpdate > lastHostUpdate)
+			{
+				var hostFile = File.ReadAllLines(hostFilePath)
+					.Select(line => Regex.Match(line, $@"127\.0\.0\.1\s+(\S+)\s*\# {motivator}"))
+					.Where(match => match.Success)
+					.Select(match => match.Groups[1].Value)
+					.ToList();
+
+				var newLines = blockList
+					.Where(line => !hostFile.Contains(line))
+					.Select(line => $"127.0.0.1 {line} # {motivator}")
+					.ToList();
+
+				File.AppendAllLines(hostFilePath, newLines);
+
+				lastHostUpdate = DateTime.Now;
+			}
+		}
+	}
+}
